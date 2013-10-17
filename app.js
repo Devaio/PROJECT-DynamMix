329f88b110fbf6e8173ad462e04cc6a537a606a0
Robert Camp <robert.camp@outlook.com>
2013-10-16 18:03:09 -0600

    jade templating started

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  * Module dependencies.
  */
 
-var express = require('express'); 
+var express = require('express');
 var router = require('./routes/router');
 var http = require('http');
 var path = require('path');
