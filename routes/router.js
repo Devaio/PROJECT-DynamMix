aae5c175c6f24d11f211deed78b49824c73d0184
Robert Camp <robert.camp@outlook.com>
2013-10-17 09:01:29 -0600

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'DynaTune' });
+  res.render('index', { title: 'DynamMix' });
 };
 
 exports.login = function(req, res){
@@ -13,5 +13,5 @@ exports.login = function(req, res){
 }
 
 exports.loggedIn = function(req, res){
-	res.render('userinfo', { username : username, title: 'DynaTune' })
+	res.render('userinfo', { username : username, title: 'DynamMix' })
 }
